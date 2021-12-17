eb9f9886004e4950bcb1576c42b99bfe0028b22b
Arthur Vianna <arthurpedrosa96.ap@gmail.com>
2021-12-17 20:33:54 -0300

    ensure that the email validator calls validator with correct email

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -20,7 +20,14 @@ describe('Email Validator', () => {
   test('should return false if validator returns false', () => {
     validator.isEmailValid = false
     const sut = makeSut()
-    const isEmailValid = sut.isValid('invalid_email')
+    const isEmailValid = sut.isValid('invalid_email@mail.com')
     expect(isEmailValid).toBe(false)
+  })
+
+  test('should call validator with correct email param', () => {
+    const sut = makeSut()
+    const requestEmail = 'valid_email@mail.com'
+    sut.isValid(requestEmail)
+    expect(validator.email).toBe(requestEmail)
   })
 })
