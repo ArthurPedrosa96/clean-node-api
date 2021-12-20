353b2987cd9698d97fbfc3a9f6c283dac46edba4
Arthur Vianna <arthurpedrosa96.ap@gmail.com>
2021-12-20 14:48:34 -0300

    ensure AuthUseCase throws error if no dependencies are provided

--- src/domain/useCases/auth-useCase.spec.js
+++ src/domain/useCases/auth-useCase.spec.js
@@ -74,6 +74,12 @@ describe('Auth UseCase', () => {
     expect(loadUserByEmailRepositorySpy.email).toBe('any_email@mail.com')
   })
 
+  test('should throw an error if no dependencies are provided', async () => {
+    const sut = new AuthUseCase()
+    const promise = sut.auth('any_email@mail.com', 'any_password')
+    expect(promise).rejects.toThrow(new MissingParamError('loadUserByEmailRepository'))
+  })
+
   test('should throw an error if no LoadUserByEmailRepository is provided', async () => {
     const sut = new AuthUseCase({})
     const promise = sut.auth('any_email@mail.com', 'any_password')
