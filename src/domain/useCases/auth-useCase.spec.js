fa88ab7979c677159e3b6ba2afeb16453538cd12
Arthur Vianna <arthurpedrosa96.ap@gmail.com>
2021-12-20 11:19:09 -0300

    created factory function for instanciating AuthUseCase object

--- src/domain/useCases/auth-useCase.spec.js
+++ src/domain/useCases/auth-useCase.spec.js
@@ -7,9 +7,14 @@ class AuthUseCase {
     }
   }
 }
+
+const makeSut = () => {
+  const sut = new AuthUseCase()
+  return sut
+}
 describe('Auth UseCase', () => {
   test('should Throw error if no email is provided', async () => {
-    const sut = new AuthUseCase()
+    const sut = makeSut()
     const promise = sut.auth()
     expect(promise).rejects.toThrow(new MissingParamError('email'))
   })
