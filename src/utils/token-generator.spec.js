2ccf2da618ca4296be3483886a454d3f35233023
Arthur Vianna <arthurpedrosa96.ap@gmail.com>
2021-12-24 10:42:58 -0300

    ensure TokenGenerator calls JWT with correct params

--- src/utils/token-generator.spec.js
+++ src/utils/token-generator.spec.js
@@ -24,4 +24,10 @@ describe('Token Generator', () => {
     const token = await sut.generate('any_id')
     expect(token).toBe(jwt.token)
   })
+
+  test('should call JWT with correct id', async () => {
+    const sut = makeSut()
+    await sut.generate('any_id')
+    expect(jwt.id).toBe('any_id')
+  })
 })
