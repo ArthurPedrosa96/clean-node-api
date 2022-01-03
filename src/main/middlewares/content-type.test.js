3b70039ce020b8f0b4012a87a87555f04b3683cf
Arthur Vianna <arthurpedrosa96.ap@gmail.com>
2022-01-03 12:21:35 -0300

    ensure xml content-type is returned if xml content-type is forced on the http request

--- src/main/middlewares/content-type.test.js
+++ src/main/middlewares/content-type.test.js
@@ -11,4 +11,15 @@ describe('Content-Type middleware', () => {
       .get('/test-content-type')
       .expect('content-type', /json/)
   })
+
+  test('should return xml content-type if xml content-type is informed', async () => {
+    app.get('/test-content-type-xml', (req, res) => {
+      res.type('xml')
+      res.send('')
+    })
+
+    await request(app)
+      .get('/test-content-type-xml')
+      .expect('content-type', /xml/)
+  })
 })
