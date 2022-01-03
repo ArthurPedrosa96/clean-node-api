73f57a2e8bd7c8befd5ab8be5f600391d64886eb
Arthur Vianna <arthurpedrosa96.ap@gmail.com>
2022-01-03 10:30:32 -0300

    ensure UpdateAccessTokenRepository throws if no userModel is provided

--- src/infra/repositories/update-access-token-repository.spec.js
+++ src/infra/repositories/update-access-token-repository.spec.js
@@ -1,3 +1,4 @@
+
 const MongoHelper = require('../helpers/mongo-helper')
 let db
 
@@ -46,5 +47,20 @@ describe('UpdateAccessToken Repository', () => {
     await sut.update(fakeUserInsertion.insertedId, 'valid_token')
     const updatedFakeUser = await userModel.findOne({ _id: fakeUser._id })
     expect(updatedFakeUser.accessToken).toBe('valid_token')
+  })
+
+  test('Should throw an error if no userModel is provided', async () => {
+    const sut = new UpdateAccessTokenRepository()
+    const userModel = db.collection('users')
+    const fakeUser = {
+      email: 'valid_email@mail.com',
+      name: 'any_name',
+      age: 50,
+      state: 'any_state',
+      password: 'hashed_password'
+    }
+    const fakeUserInsertion = await userModel.insertOne(fakeUser)
+    const promise = sut.update(fakeUserInsertion.insertedId, 'valid_token')
+    expect(promise).rejects.toThrow()
   })
 })
