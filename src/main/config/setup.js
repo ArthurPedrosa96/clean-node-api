0f20190888782d895db89d7ba19185783dd42efc
Arthur Vianna <arthurpedrosa96.ap@gmail.com>
2022-01-03 12:16:52 -0300

    ensure the default content-type is json

--- src/main/config/setup.js
+++ src/main/config/setup.js
@@ -1,7 +1,9 @@
+const contentType = require('../middlewares/content-type')
 const cors = require('../middlewares/cors')
 const jsonParser = require('../middlewares/json-parser')
 module.exports = app => {
   app.disable('x-powered-by')
   app.use(cors)
   app.use(jsonParser)
+  app.use(contentType)
 }
